85113057e92b837ab70370e916224ac3dd9167fe
Andrey Correia Gomes <andreycgomes7@gmail.com>
2025-02-17 19:57:30 -0300

    alteracoes gerais

---
+++ Calculos.Basicos/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace andrey_vetor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                double n1, n2, r;
+                Console.Write("Digite 1º Valor: ");
+                n1 = Convert.ToDouble(Console.ReadLine());
+                Console.Write("Digite 2º Valor: ");
+                n2 = Convert.ToDouble(Console.ReadLine());
+                r = n1 + n2;
+                Console.WriteLine("Soma: " + r);
+                r = n1 - n2;
+                Console.WriteLine("Subtração: " + r);
+                r = n1 / n2;
+                Console.WriteLine("Divisão: " + r);
+                r = n1 * n2;
+                Console.WriteLine("Multiplicação: " + r);
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,12 @@
+# Programas-feitos-em-Csharsp-
+Este repositório contém diversos exemplos de código e estruturas utilizadas em programação com C#. Os exemplos abordam conceitos e estruturas básicas, proporcionando um material útil para aprendizado e referência.
+
+### 🛠 Tecnologias Utilizadas
+Linguagem: C#
+<br>IDEs recomendadas: Visual Studio, Visual Studio Code
+ 
+ ### 📂 Estrutura do Repositório
+ 📂 Fundamentos – Exemplos básicos, como entrada e saída de dados, variáveis e operadores.
+ <br>📂 Estruturas de Controle – Implementações de if/else, switch, for, while e foreach.
+
+📌 Fique à vontade para contribuir, sugerir melhorias ou relatar problemas!
