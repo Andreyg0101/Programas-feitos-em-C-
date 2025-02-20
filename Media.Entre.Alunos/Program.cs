eb6aeffc533ccd8e26c3d69ce0cc4b6c34cd235f
Andrey Correia Gomes <andreycgomes7@gmail.com>
2025-02-20 10:39:07 -0300

    alteração no codigo

--- Media.Entre.Alunos/Program.cs
+++ Media.Entre.Alunos/Program.cs
@@ -6,15 +6,17 @@ namespace Media_entre_alunos
     {
         static void Main(string[] args)
         {
-            double n1, n2, n3, r;
-            Console.Write("Digite o 1° número: ");
-            n1 = Convert.ToDouble(Console.ReadLine());
-            Console.Write("Digite o 2° número: ");
-            n2 = Convert.ToDouble(Console.ReadLine());
-            Console.Write("Digite o 3° número: ");
-            n3 = Convert.ToDouble(Console.ReadLine());
-            r = (n1 + n2 + n3) / 3;
-            Console.WriteLine("A média aritmética dos numeros é: " + r);
+            double N1, N2, N3, media;
+            Console.WriteLine("Escreva as notas do aluno: ");
+            Console.WriteLine("1º nota: ");
+            N1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("2º nota: ");
+            N2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("3º nota: ");
+            N3 = Convert.ToDouble(Console.ReadLine());
+            media = (N1 + N2 + N3);
+            Console.WriteLine("A média do aluno é: ");
+            Console.WriteLine(media);
 
             Console.ReadKey();
         }
