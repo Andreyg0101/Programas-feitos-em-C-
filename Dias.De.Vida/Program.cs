697fd61e5b3d0e6695836c00d0d58425f51fc85a
Andrey Correia Gomes <andreycgomes7@gmail.com>
2025-02-20 10:35:51 -0300

    repositorio dias de vida

---
+++ Dias.De.Vida/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Dias.De.Vida
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double Mes, Anos, Dias, r;
+            Console.WriteLine("Digite quantos anos, meses e dias você tem: ");
+            Console.WriteLine("Anos: ");
+            Anos = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Meses: ");
+            Mes = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("dias: ");
+            Dias = Convert.ToDouble(Console.ReadLine());
+            r = (Anos * 365) + (Mes * 30) + Dias;
+            Console.WriteLine("Você tem: " + r + " " + "dias de vida");
+
+            Console.ReadKey();
+        }
+    }
+}
