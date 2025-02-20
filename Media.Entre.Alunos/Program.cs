66ecf518f8e781fc31c92624ed9118c1288fba64
Andrey Correia Gomes <andreycgomes7@gmail.com>
2025-02-20 10:25:13 -0300

    adição de de mais um repositorio

--- Media.Entre.Alunos/Program.cs
+++ Media.Entre.Alunos/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Media_entre_alunos
 {
@@ -10,6 +6,17 @@ namespace Media_entre_alunos
     {
         static void Main(string[] args)
         {
+            double n1, n2, n3, r;
+            Console.Write("Digite o 1° número: ");
+            n1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Digite o 2° número: ");
+            n2 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Digite o 3° número: ");
+            n3 = Convert.ToDouble(Console.ReadLine());
+            r = (n1 + n2 + n3) / 3;
+            Console.WriteLine("A média aritmética dos numeros é: " + r);
+
+            Console.ReadKey();
         }
     }
 }
